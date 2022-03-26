ac1f7fe069552bd2e09ecdebf005a8ea1d970677
BrainStone <yannick@tedworld.de>
2022-03-26 03:15:32 +0100

    Basic project setup

---
+++ src/mod_math.cpp
@@ -0,0 +1,12 @@
+// mod_math.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "mod_math.hpp"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
